aef64d11520dd9e8a40f5de3e73ca0a7cc80909c
Gary Anderson <ganderson@seattleacademy.org>
2015-05-19 08:36:36 -0700

    add app.js index.js

---
+++ app.js
@@ -0,0 +1,4 @@
+var circle = require('./index.js');
+console.log( 'The area of a circle of radius 4 is '
+           + circle.area(4));
+console.log('The circumference of a circle with radius 4 is',circle.circumference(4));
